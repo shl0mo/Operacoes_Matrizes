dadc95557699f6c5a5e704145d3953715edd36a7
shl0mo <salomao.dacostacruz@gmail.com>
2021-12-19 23:52:39 -0300

    outline

--- JS/scripts.js
+++ JS/scripts.js
@@ -8,6 +8,20 @@ function func_mudaSinal() {
 	}
 }
 
+const round = (num, places) => {
+	if (!("" + num).includes("e")) {
+		return +(Math.round(num + "e+" + places)  + "e-" + places);
+	} else {
+		let arr = ("" + num).split("e");
+		let sig = ""
+		if (+arr[1] + places > 0) {
+			sig = "+";
+		}
+
+		return +(Math.round(+arr[0] + "e" + sig + (+arr[1] + places)) + "e-" + places);
+	}
+}
+
 function func_volta() {
 	$('#section_resultado').animate({left: "100px"}, 100, () => {$('#section_resultado').remove()});
 	$('#section_dados').animate({left: "0px"}, 100, () => {$('#section_dados').show()});
@@ -30,89 +44,117 @@ function func_volta() {
 
 function func_verificaMatriz(idMatriz) {
 	$('#' + idMatriz).css("box-shadow", "none");
-	let array = document.getElementById(idMatriz).value.split('\n');
-	let novoArray = [];
-	for (i = 0; i < array.length; i++) {
-		let linha = [];
-		for (j = 0; j < array[i].length; j++) {
-			if (parseFloat(array[i][j])/1 == parseFloat(array[i][j])) {
-				linha.push(parseFloat(array[i][j]));
-			} else if ((array[i][j] == " " && j == 0) || array[i][j] == '') {
-				linha.push(array[i][j]);
+	//Passo 1:
+	let array1 = (document.getElementById(idMatriz).value).split('\n');
+	let array2 = [];
+	let array = [];
+	let arrayFinal = [];
+	//Passo 2:
+	for (i = 0; i < array1.length; i++) {
+		let strNum = (array1[i]).split(" ");
+		array2.push(strNum);
+	}
+	for (i = 0; i < array2.length; i++) {
+		for (j = 0; j < array2[i].length; j++) {
+			let pont = 0;
+			for (k = 0; k < array2[i][j].length; k++) {
+				if (parseFloat(array2[i][j][k])/1 != parseFloat(array2[i][j][k])/1 && array2[i][j][k] != '.') {
+					if (array2[i][j][k] == ',') {
+						$('#' + idMatriz).css("box-shadow", "0px 0px 0.3em red");
+						alert("As casas decimais devem ser separadas por ponto e não por vírgula");
+						return false;
+					} else {
+						$('#' + idMatriz).css("box-shadow", "0px 0px 0.3em red");
+						alert("Caractere inválido no campo de dados");
+						return false;
+					}
+				}
+				if (array2[i][j][k] == '.') {
+					pont++;
+				}
+			}
+			if (pont > 1) {
+				$('#' + idMatriz).css("box-shadow", "0px 0px 0.3em red");
+				alert("Caractere inválido entre números - dois pontos de casas decimais em um número");
+				return false;
 			}
 		}
-		novoArray.push(linha);
 	}
-	if (novoArray == '') {
+	//alert(array2);
+	//Passo 3:
+	for (i = 0; i < array2.length; i++) {
+		let linha = [];
+		for (j = 0; j < array2[i].length; j++) {
+			if (parseFloat(array2[i][j])/1 == parseFloat(array2[i][j])) {
+				let strNumero = array2[i][j].toString();
+				if (strNumero.indexOf('.') != -1) {
+					strNumero = strNumero.split('.');
+					let inteiro = parseFloat(strNumero[0]);
+					let real = "0." + strNumero[1];
+					real = parseFloat(real);
+					let numero = inteiro + real;
+					numero = parseFloat(numero);
+					array.push(numero);
+				} else {
+					array.push(parseFloat(array2[i][j]));	
+				}
+			}
+		}
+		array.push(linha);
+	}
+	if (array2 == '') {
 		alert("Preencha os devidos campos de dados para continuar");
 		$('#' + idMatriz).css("box-shadow", "0px 0px 0.3em red");
 		return false;
 	}
-	let indiceVazia = -1;
 	let primeiroIndice = 0;
-	for (i = 0; i < novoArray.length; i++) {
-		if (novoArray[i][0] == ' ') {
-			alert("O primeiro elemento de uma linha não pode ser espaço ou enter");
+	for (i = 0; i < array2.length; i++) {
+		if (array2[i][0] == '' && array2[i] != '') {
+			alert("O primeiro elemento de uma linha não pode ser um espaço");
 			$('#' + idMatriz).css("box-shadow", "0px 0px 0.3em red");
 			return false;
-		}
-		if (novoArray[i].length == 0) {
-			if (primeiroIndice == 0) {
-				indiceVazia = i;
-				primeiroIndice++;
-			}
-			for (j = i + 1; j < novoArray.length; j++) {
-				if (novoArray[j].length != 0) {
+		} else if (array2[i] == '') {
+			for (j = i; j < array2.length; j++) {
+				if (array2[j] != '') {
 					alert("As linhas devem estar uma imediatamente embaixo da outra");
 					$('#' + idMatriz).css("box-shadow", "0px 0px 0.3em red");
 					return false;
 				}
 			}
 		}
-		if (novoArray[i].length != 0) {
-			if (novoArray[i].length != novoArray[0].length) {
-				alert("Todas as linhas de uma matriz devem ter o mesmo número de colunas");
-				$('#' + idMatriz).css("box-shadow", "0px 0px 0.3em red");
-				return false;
-			}
-		}
 	}
-	if (indiceVazia != -1) {
-		let quantidade = (array.length - 1) - indiceVazia;
-		array.splice(indiceVazia, quantidade);
-		array.splice((array.length - 1), 1);
-	}
-	let arrayFinal = [];
-	for (i = 0; i < array.length; i++) {
+	for (i = 0; i < array2.length; i++) {
 		let linha = [];
-		for (j = 0; j < array[i].length; j++) {
-			if (parseFloat(array[i][j])/1 == parseFloat(array[i][j]) && array[i][j] != '\n') {
-				if (j > 0 && array[i][j - 1] == "-") {
-					linha.push(parseFloat(array[i][j]*(-1)));
-					j++;
-				} else {
-					linha.push(parseFloat(array[i][j]));
+		if (array2[i] != '') {
+			for (j = 0; j < array2[i].length; j++) {
+				if (parseFloat(array2[i][j])/1 == parseFloat(array2[i][j])) {
+					linha.push(array2[i][j]);
 				}
 			}
+			arrayFinal.push(linha);
 		}
-		arrayFinal.push(linha);
+	}
+	for (i = 0; i < arrayFinal.length; i++) {
+		if (arrayFinal[i].length != arrayFinal[0].length) {
+			alert("Todas as linhas de uma matriz devem ter o mesmo número de colunas");
+			$('#' + idMatriz).css("box-shadow", "0px 0px 0.3em red");
+			return false;
+		}
 	}
 	return arrayFinal;
 }
-
 
 function func_soma(arrayMatriz_1, arrayMatriz_2, idJQ) {
 	for (i = 0; i < arrayMatriz_1.length; i++) {
 		let strResult = "";
 		for (j = 0; j < arrayMatriz_1[i].length; j++) {
-			if (arrayMatriz_1[i][j] == "\n" || arrayMatriz_1[i][j] == " ") {
-				if (arrayMatriz_1[i][j] == " " && j != (arrayMatriz_1[i].length - 1)) {
-					strResult = strResult + arrayMatriz_1[i][j];
-				}
-			} else {
-				let soma = parseFloat(arrayMatriz_1[i][j]) + parseFloat(arrayMatriz_2[i][j]);
-				strResult = strResult + soma;
-			} 
+			soma = parseFloat(arrayMatriz_1[i][j]) + parseFloat(arrayMatriz_2[i][j]);
+			soma = soma.toString();
+			if (soma.indexOf('.') != -1) {
+				soma = parseFloat(soma);
+				soma = round(soma, 3);
+			}
+			strResult += soma + " ";
 		}
 		$(idJQ).append(strResult + "\n");
 	}
@@ -122,14 +164,13 @@ function func_subtrai(arrayMatriz_1, arrayMatriz_2, idJQ) {
 	for (i = 0; i < arrayMatriz_1.length; i++) {
 		let strResult = "";
 		for (j = 0; j < arrayMatriz_1[i].length; j++) {
-			if (arrayMatriz_1[i][j] == "\n" || arrayMatriz_1[i][j] == " ") {
-				if (arrayMatriz_1[i][j] == " " && j != (arrayMatriz_1[i].length - 1)) {
-					strResult = strResult + arrayMatriz_1[i][j];
-				}
-			} else {
-				let soma = parseFloat(arrayMatriz_1[i][j]) - parseFloat(arrayMatriz_2[i][j]);
-				strResult = strResult + soma;
+			soma = parseFloat(arrayMatriz_1[i][j]) - parseFloat(arrayMatriz_2[i][j]);
+			soma = soma.toString();
+			if (soma.indexOf('.') != -1) {
+				soma = parseFloat(soma);
+				soma = round(soma, 3);
 			}
+			strResult += soma + " ";
 		}
 		$(idJQ).append(strResult + "\n");
 	}
@@ -142,7 +183,12 @@ function func_multiplica(arrayMatriz_1, arrayMatriz_2, idJQ) {
 		for (k = 0; k < arrayMatriz_2[0].length; k++) {
 			let soma = 0;
 			for (j = 0; j < arrayMatriz_1[0].length; j++) {
-				soma += arrayMatriz_1[i][j] * arrayMatriz_2[j][k];
+				soma = parseFloat(soma) + parseFloat(arrayMatriz_1[i][j]) * parseFloat(arrayMatriz_2[j][k]);
+				soma = soma.toString();
+				if (soma.indexOf('.') != -1) {
+					soma = parseFloat(soma);
+					soma = round(soma, 3);
+				}
 			}
 			strResult = strResult + soma + " ";
 		}
@@ -155,12 +201,16 @@ function func_calcula() {
 	if (document.getElementById('selectOp').value == "-- Operação --") {
 		alert("Escolha a operação a ser realizada");
 	} else {
-		arrayMatriz_1 = document.getElementById('matriz1').value.split("\n");
-		arrayMatriz_2 = document.getElementById('matriz2').value.split("\n");
+		arrayMatriz_1 = (document.getElementById('matriz1').value).split("\n");
+		arrayMatriz_2 = (document.getElementById('matriz2').value).split("\n");
 		let arrayFinal1 = func_verificaMatriz('matriz1');
+		if (arrayFinal1 == false) {
+			return false;
+		}
 		let arrayFinal2 = func_verificaMatriz('matriz2');
-		alert(arrayFinal1);
-		alert(arrayFinal2);
+		if (arrayFinal2 == false) {
+			return false;
+		}
 		if (arrayFinal1 != false && arrayFinal2 != false) {
 			if ((arrayFinal1.length != arrayFinal2.length) && document.getElementById('selectOp').value != "Multiplicação") {
 				alert("As matrizes devem ter o mesmo número de linhas");
@@ -180,9 +230,9 @@ function func_calcula() {
 			} else {
 				$('body').append('<section id="section_resultado" style="display: none"><button style="background-image: url(IMG/volt.png);position: absolute" id="voltar" readonly onclick="func_volta()">Voltar</button><br/><h1>Resultado:</h1><br/><textarea rows="10" cols="20" id="textarea_resultado" class="textareaMatriz" readonly></textarea></section>');
 				if (document.getElementById('selectOp').value == "Soma") {
-					func_soma(arrayMatriz_1, arrayMatriz_2, 'textarea');
+					func_soma(arrayFinal1, arrayFinal2, 'textarea');
 				} else if (document.getElementById('selectOp').value == "Subtração") {
-					func_subtrai(arrayMatriz_1, arrayMatriz_2, 'textarea');
+					func_subtrai(arrayFinal1, arrayFinal2, 'textarea');
 				} else {
 					func_multiplica(arrayFinal1, arrayFinal2, 'textarea');
 				}
