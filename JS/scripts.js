2d74fee89e48cf0d4d3822cd46e4ecf14a2d1d8a
shl0mo <salomao.dacostacruz@gmail.com>
2021-12-21 23:21:33 -0300

    resultado-1

--- JS/scripts.js
+++ JS/scripts.js
@@ -228,7 +228,7 @@ function func_calcula() {
 				$('#matriz2').css("box-shadow", "0px 0px 0.3em red");
 				return false;
 			} else {
-				$('body').append('<section id="section_resultado" style="display: none"><button style="background-image: url(IMG/volt.png);position: absolute" id="voltar" readonly onclick="func_volta()">Voltar</button><br/><h1>Resultado:</h1><br/><textarea rows="10" cols="20" id="textarea_resultado" class="textareaMatriz" readonly></textarea></section>');
+				$('body').append('<section id="section_resultado" style="display: none"><button id="voltar" readonly onclick="func_volta()"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left-circle" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/></svg> Voltar</button><br/><h1>Resultado:</h1><br/><textarea rows="10" cols="20" id="textarea_resultado" class="textareaMatriz" readonly></textarea></section>');
 				if (document.getElementById('selectOp').value == "Soma") {
 					func_soma(arrayFinal1, arrayFinal2, 'textarea');
 				} else if (document.getElementById('selectOp').value == "Subtração") {
